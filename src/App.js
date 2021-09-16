db764a7c57223559170f7bbc3a7736e184654a57
Wyatt726 <wyatt726@aol.com>
2021-09-16 16:11:42 -0400

    created sidebar,search, similar videos, and video display

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import React, from 'react';
-import axios from 'axios'
+import React,{Component} from 'react';
+import axios from 'axios';
 
 class App extends Component {
   constructor(props) {
