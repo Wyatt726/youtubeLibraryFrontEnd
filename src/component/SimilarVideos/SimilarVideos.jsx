ef70005a1a691a9236f3a108cdb77392e538d048
Wyatt726 <wyatt726@aol.com>
2021-09-16 16:08:55 -0400

    created sidebar,search, similar videos, and video display

---
+++ src/component/SimilarVideos/SimilarVideos.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+function SimilarVideos(props){
+    return(
+        <h1>Similar videos display</h1>
+    )
+}
+
+export default SimilarVideos;
---
+++ src/component/VideoDisplay/VideoDisplay.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./VideoDisplay.css";
+
+function DisplayVideo (props){
+    return (
+        <h1>This is Video Location</h1>
+    )
+}
+
+export default DisplayVideo;
